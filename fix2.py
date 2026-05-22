b5204df5c9a92d92ff5c5e399da87ed02a5b870a
Nguyễn Ích Thắng <tn8989607@gmail.com>
2026-05-22 23:23:03 +0700

    V3 - Game hoan chinh: pixel art, boss, HP bar, background dong

---
+++ fix2.py
@@ -0,0 +1,15 @@
+﻿path = 'C:/game-in-cpp-full-course/src/gameLayer/gameLayer.cpp'
+with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+    lines = f.readlines()
+audio_keywords = ['raudio', 'Sound', 'Music', 'Audio', 'LoadSound', 'PlaySound', 
+                  'IsSoundPlaying', 'SetSoundVolume', 'LoadMusicStream', 
+                  'PlayMusicStream', 'UpdateMusicStream', 'shootSound', 'music']
+new_lines = []
+for line in lines:
+    stripped = line.strip()
+    if any(k in stripped for k in audio_keywords) and not stripped.startswith('//'):
+        line = '//' + line
+    new_lines.append(line)
+with open(path, 'w', encoding='utf-8') as f:
+    f.writelines(new_lines)
+print('Done!')
